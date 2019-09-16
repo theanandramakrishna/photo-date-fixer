72e2b2b45fa56009963be02383dc04023ede7791
Anand Ramakrishna <theanandramakrishna@gmail.com>
2019-09-16 12:01:57 -0700

    fixes

--- photoedit/src/main/java/com/anand/photoedit/AlbumEditor.java
+++ photoedit/src/main/java/com/anand/photoedit/AlbumEditor.java
@@ -1,9 +1,13 @@
 package com.anand.photoedit;
 
+import java.util.GregorianCalendar;
+
 import com.google.photos.library.v1.PhotosLibraryClient;
 import com.google.photos.library.v1.internal.InternalPhotosLibraryClient.SearchMediaItemsPagedResponse;
 import com.google.photos.types.proto.Album;
 import com.google.photos.types.proto.MediaItem;
+import com.google.photos.types.proto.MediaMetadata;
+import com.google.protobuf.Timestamp;
 
 public class AlbumEditor
 {
@@ -22,6 +26,22 @@ public class AlbumEditor
 
         for (MediaItem mediaItem : response.iterateAll())
         {
+            MediaMetadata metadata = mediaItem.getMediaMetadata();
+            Timestamp createTime = metadata.getCreationTime();
+
+            GregorianCalendar cal = new GregorianCalendar();
+            cal.setTimeInMillis(createTime.getSeconds() * 1000);
+            if (cal.get(GregorianCalendar.YEAR) != 2019)
+            {
+                continue;
+            }
+
+            //
+            // We found an item with a create date to this year.  Needs to be edited
+            //
+
+            String fileName = mediaItem.getFilename();
+
             
         }
     }
